487a58a5fb12a6ee58357bc02a712943516a98b0
Pranav Athanimath <pranavathanimath@gmail.com>
2026-01-12 14:34:24 +0100

    Fix typo: Rename sypnopsis to Synopsis in data and code

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,23 +1,27 @@
 import pandas as pd
 
+
 class AnimeDataLoader:
-    def __init__(self,original_csv:str,processed_csv:str):
+    def __init__(self, original_csv: str, processed_csv: str):
         self.original_csv = original_csv
         self.processed_csv = processed_csv
 
     def load_and_process(self):
-        df = pd.read_csv(self.original_csv , encoding='utf-8' , on_bad_lines='skip').dropna()
+        df = pd.read_csv(self.original_csv, encoding='utf-8',
+                         on_bad_lines='skip').dropna()
 
-        required_cols = {'Name' , 'Genres','sypnopsis'}
+        required_cols = {'Name', 'Genres', 'Synopsis'}
 
         missing = required_cols - set(df.columns)
         if missing:
             raise ValueError("Missing column  in CSV File")
-        
+
         df['combined_info'] = (
-            "Title: " + df["Name"] + " Overview: " +df["sypnopsis"] + "Genres : " + df["Genres"]
+            "Title: " + df["Name"] + " Overview: " +
+            df["Synopsis"] + "Genres : " + df["Genres"]
         )
 
-        df[['combined_info']].to_csv(self.processed_csv , index=False,encoding='utf-8')
+        df[['combined_info']].to_csv(
+            self.processed_csv, index=False, encoding='utf-8')
 
         return self.processed_csv
